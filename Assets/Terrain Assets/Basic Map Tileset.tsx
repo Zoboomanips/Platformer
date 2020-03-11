56e76b10b0d615911c9bc6c50dd8b4f7b0ecf49b
wannabemario <60630382+wannabemario@users.noreply.github.com>
2020-03-10 22:27:26 -0500

    Added Basic Map Files
    Libraries and support for Tiled maps: https://doc.mapeditor.org/en/latest/reference/support-for-tmx-maps/
    Introduction to Tiled: https://doc.mapeditor.org/en/latest/manual/introduction/

---
+++ Assets/Terrain Assets/Basic Map Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Basic Map Tileset" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="../../Downloads/Basic Map Tileset.png" width="128" height="160"/>
+</tileset>
