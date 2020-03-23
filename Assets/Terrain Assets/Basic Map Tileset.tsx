7ce0a5379b4c29c56995af7a3ec7acbf3b9336e6
andrew3ng <32119106+andrew3ng@users.noreply.github.com>
2020-03-23 16:20:14 -0500

    Add files via upload

---
+++ Assets/Terrain Assets/Basic Map Tileset.tsx
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Basic Map Tileset" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="Basic Map Tileset.png" width="128" height="160"/>
+ <tile id="0" type="Inner Ground">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="InnerGround" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1" type="Ground1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="Ground Tile" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2" type="GroundRightEdge">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="GroundRightEdge" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" type="LeftWall1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="LeftWall1" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" type="LeftWall2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="LeftWall2" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" type="LeftWall3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="LeftWall3" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6" type="LeftWall4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="LeftWall4" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7" type="LeftCorner">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="LeftCorner" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8" type="GroundLeftEdge">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="GroundLeftEdge" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9" type="RightWall1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="RightWall1" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10" type="RightWall2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="RightWall2" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11" type="RightWall3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="RightWall3" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12" type="RightWall4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="RightWall4" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" type="RightCorner">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="RightCorner" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14" type="Box">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="Box" type="Solid" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15" type="BoxBg">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="BoxBg" type="BgObj" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="2" name="Platform" type="Platform" x="0" y="0" width="32" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="17" type="SpawnPoint">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="SpawnPoint" type="SpawnPoint" x="16" y="32">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
