87f35a38db6ece95bbf0b9d305a4b9a2c9753dae
Zoboomanips <38367374+Zoboomanips@users.noreply.github.com>
2020-03-23 16:02:14 -0500

    New Singleton and Stats

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    protected GameManager() { }
+
+    public int pla1cha = 1;
+    public int pla2cha = 2;
+    public int pla3cha = 3;
+    public int pla4cha = 4;
+
+    public bool pla1ex = true;
+    public bool pla2ex = true;
+    public bool pla3ex = false;
+    public bool pla4ex = false;
+
+}
--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -7,12 +7,52 @@ public class Stats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player Player1 = new player(1, GameManager.Instance.pla1cha, GameManager.Instance.pla1ex);
+        player Player2 = new player(2, GameManager.Instance.pla2cha, GameManager.Instance.pla2ex);
+        player Player3 = new player(3, GameManager.Instance.pla3cha, GameManager.Instance.pla3ex);
+        player Player4 = new player(4, GameManager.Instance.pla4cha, GameManager.Instance.pla4ex);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+}
+
+class player
+{
+    public int num;
+    public int character;
+    public int souls;
+    public int health;
+    public bool exist;
+
+    public player(int n, int c, bool e)
+    {
+        num = n;
+        character = c;
+        exist = e;
+        health = 100;
+        souls = 1;
+    }
+    public void hit(int val)
+    {
+        health = health - val;
+        if (health <= 0)
+        {
+            death();
+        }
+    }
+
+    public void death()
+    {
+        souls = (int) Mathf.Floor(souls/2) + 1;
+        health = 100;
+    }
+
+    public void gain(int soul)
+    {
+        souls += soul;
     }
 }
