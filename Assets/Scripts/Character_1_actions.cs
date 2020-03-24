6ecb8260c0700ea47a883970b6097584a2ee0596
Zoboomanips <38367374+Zoboomanips@users.noreply.github.com>
2020-03-24 12:22:28 -0500

    Updated terrain and skeleton code

---
+++ Assets/Scripts/Character_1_actions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_1_actions : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void attack(int seq)
+    {
+
+    }
+
+    public void special()
+    {
+
+    }
+}
--- Assets/Scripts/Player_1_move.cs
+++ Assets/Scripts/Player_1_move.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player_1_move : MonoBehaviour
 {
+    public GameObject chara;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,32 @@ public class Player_1_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (true/*move right button*/)
+        {
+            //move
+        }
+        if (true/*move left button*/)
+        {
+            //move
+        }
+        if (true/*jump button*/)
+        {
+            //jump
+        }
+        if (true/*shield button*/)
+        {
+            //shield
+        }
+
+        if (true/*attack button*/)
+        {
+            plaAttack(); 
+        }
+
+    }
+
+    public void plaAttack()
+    {
+        //chara.attack(1);
     }
 }
