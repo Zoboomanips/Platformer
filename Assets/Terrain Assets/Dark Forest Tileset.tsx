430c51ed31528ea91af4514dda6485deaaad9cc2
andrew3ng <andreweng1117@gmail.com>
2020-04-20 01:32:04 -0500

    Added Dark Forest Map

---
+++ Assets/Terrain Assets/Dark Forest Tileset.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Dark Forest Tileset" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="Dark Woods Tileset.png" width="192" height="192"/>
+ <tile id="6" type="Solid">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 31.8808,0.0644058 31.9453,31.752"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 31.8808,0 0,31.8808"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="16" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="17" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="27" type="Climbable">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="28" type="Climbable">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="30" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
