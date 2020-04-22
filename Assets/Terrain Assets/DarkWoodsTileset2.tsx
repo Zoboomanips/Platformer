5d09870cdd9a59bcb397688a09e7aab05ed109c6
andrew3ng <andreweng1117@gmail.com>
2020-04-22 14:18:54 -0500

    Reworked Dark Woods Map

---
+++ Assets/Terrain Assets/DarkWoodsTileset2.tsx
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="DarkWoodsTileset2" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="Dark Woods Tileset 2.png" width="192" height="192"/>
+ <tile id="6" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 31.9824,0.17605 31.9237,32.0998"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 31.865,0.117367 31.9237,32.0411"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0">
+    <polygon points="0,0 -31.8662,0 -31.9249,31.6901"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0">
+    <polygon points="0,0 -31.9836,0.0586854 -31.8662,31.7488"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="18" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="31" type="Ladder">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="32" type="Ladder">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="33" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="34" type="Solid">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
