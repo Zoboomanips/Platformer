464ab2bd2e9abba6261947701a3092a8aeaa5254
Zoboomanips <38367374+Zoboomanips@users.noreply.github.com>
2020-04-25 11:46:09 -0500

    Character 3 assets added, Map implemented

--- Assets/Scripts/Character_actions.cs
+++ Assets/Scripts/Character_actions.cs
@@ -33,7 +33,28 @@ public class Character_actions : MonoBehaviour
 
     public void attack1(int seq)
     {
-
+        if (seq < 4)
+        {
+            if (gameObject.GetComponent<SpriteRenderer>().flipX)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+        else
+        {
+            if (gameObject.GetComponent<SpriteRenderer>().flipX)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
     }
 
     public void special1()
@@ -47,22 +68,22 @@ public class Character_actions : MonoBehaviour
         {
             if (gameObject.GetComponent<SpriteRenderer>().flipX)
             {
-                StartCoroutine(Att(.2f, true));
+                StartCoroutine(Att2(.2f, true));
             }
             else
             {
-                StartCoroutine(Att(.2f, false));
+                StartCoroutine(Att2(.2f, false));
             }
         }
         else
         {
             if (gameObject.GetComponent<SpriteRenderer>().flipX)
             {
-                StartCoroutine(Att4(.2f, true));
+                StartCoroutine(Att24(.2f, true));
             }
             else
             {
-                StartCoroutine(Att4(.2f, false));
+                StartCoroutine(Att24(.2f, false));
             }
         }
     }
@@ -71,11 +92,11 @@ public class Character_actions : MonoBehaviour
     {
         if (gameObject.GetComponent<SpriteRenderer>().flipX)
         {
-            StartCoroutine(Spec(1f, true));
+            StartCoroutine(Spec2(1f, true));
         }
         else
         {
-            StartCoroutine(Spec(1f, false));
+            StartCoroutine(Spec2(1f, false));
         }
     }
 
@@ -104,20 +125,20 @@ public class Character_actions : MonoBehaviour
         yield return new WaitForSeconds(sec);
     }
 
-    IEnumerator Att(float sec, bool flip)
+    IEnumerator Att2(float sec, bool flip)
     {
         yield return new WaitForSeconds(sec);
         Instantiate(dag, gameObject.GetComponent<Rigidbody2D>().transform.position, gameObject.GetComponent<Rigidbody2D>().transform.rotation).GetComponent<SpriteRenderer>().flipX = flip;
     }
 
-    IEnumerator Att4(float sec, bool flip)
+    IEnumerator Att24(float sec, bool flip)
     {
         yield return new WaitForSeconds(sec);
         Instantiate(dag, new Vector3(gameObject.GetComponent<Rigidbody2D>().transform.position.x, gameObject.GetComponent<Rigidbody2D>().transform.position.y), gameObject.GetComponent<Rigidbody2D>().transform.rotation).GetComponent<SpriteRenderer>().flipX = flip;
         Instantiate(dag, new Vector3(gameObject.GetComponent<Rigidbody2D>().transform.position.x, gameObject.GetComponent<Rigidbody2D>().transform.position.y + 0.1f), gameObject.GetComponent<Rigidbody2D>().transform.rotation).GetComponent<SpriteRenderer>().flipX = flip;
     }
 
-    IEnumerator Spec(float sec, bool flip)
+    IEnumerator Spec2(float sec, bool flip)
     {
         yield return new WaitForSeconds(sec);
         Instantiate(stunDag, gameObject.GetComponent<Rigidbody2D>().transform.position, gameObject.GetComponent<Rigidbody2D>().transform.rotation).GetComponent<SpriteRenderer>().flipX = flip;
